0a86ef01ab1d8c8ccb4af967971d0c9c73c5356a
xuziyang <767637918@qq.com>
2022-03-08 15:57:01 +0800

    添加HttpPostClient

---
+++ src/main/java/client/HttpGetClient.java
@@ -0,0 +1,38 @@
+package client;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class HttpGetClient {
+    
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("127.0.0.1", 8080);
+        
+        OutputStream out = socket.getOutputStream();
+        InputStream in = socket.getInputStream();
+        
+        out.write("GET ".getBytes());
+        out.flush();
+        out.write("/hello HTTP/1.0\r\n".getBytes());
+        out.write("Accept: */*\r\n".getBytes());
+        out.write("Host: 127.0.0.1\r\n".getBytes());
+        out.write("Connection: Close\r\n\r\n".getBytes());
+        out.flush();
+        
+        // read the response
+        StringBuffer response = new StringBuffer();
+        byte[] buffer = new byte[4096];
+        int bytes_read;
+        
+        // Reads HTTP response
+        while ((bytes_read = in.read(buffer, 0, 4096)) != -1) {
+            // Print server's response
+            for (int i = 0; i < bytes_read; i++)
+                response.append((char) buffer[i]);
+        }
+        System.out.println(response);
+        
+        socket.close();
+    }
+}
--- src/main/java/client/HttpPostClient.java
+++ src/main/java/client/HttpPostClient.java
@@ -4,27 +4,25 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class HttpClient {
-    
+public class HttpPostClient {
     public static void main(String[] args) throws Exception {
         Socket socket = new Socket("127.0.0.1", 8080);
-        
+
         OutputStream out = socket.getOutputStream();
         InputStream in = socket.getInputStream();
-        
-        out.write("GET ".getBytes());
-        out.flush();
-        out.write("/hello HTTP/1.0\r\n".getBytes());
-        out.write("Accept: */*\r\n".getBytes());
+
+        out.write("POST /hello HTTP/1.0\r\n".getBytes());
+        out.write("Content-Type: application/x-www-form-urlencoded\r\n".getBytes());
         out.write("Host: 127.0.0.1\r\n".getBytes());
-        out.write("Connection: Close\r\n\r\n".getBytes());
+        out.write("Content-Length: 8\r\n\r\n".getBytes());
+        out.write("name=xzy".getBytes());
         out.flush();
-        
+
         // read the response
         StringBuffer response = new StringBuffer();
         byte[] buffer = new byte[4096];
         int bytes_read;
-        
+
         // Reads HTTP response
         while ((bytes_read = in.read(buffer, 0, 4096)) != -1) {
             // Print server's response
@@ -32,7 +30,7 @@ public class HttpClient {
                 response.append((char) buffer[i]);
         }
         System.out.println(response);
-        
+
         socket.close();
     }
 }
